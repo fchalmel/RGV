d3bcac64b97b8a569b5dd0fe365948190f4c5683
fchalmel <32904249+fchalmel@users.noreply.github.com>
2017-10-31 16:57:47 +0100

    RGV2 1rst draft
    RGV2 1rst draft based on a TOXsIgN structure

---
+++ rgv/webapp/app/scripts/rgv.js
@@ -0,0 +1,338 @@
+/*global  angular:false */
+/*jslint sub: true, browser: true, indent: 4, vars: true, nomen: true */
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('rgv', ['rgv.resources','angular-carousel', 'ngDialog', 'ngHandsontable', 'ngTableToCsv', 'ngFileUpload', 'ngSanitize', 'ngCookies', 'angular-js-xlsx', 'ngRoute','angular-venn', 'ui.bootstrap', 'datatables', 'ui.tree', 'uuid', 'ngTable','angucomplete-alt']).
+
+config(['$routeProvider','$logProvider',
+    function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'appCtrl'
+        });
+
+        $routeProvider.when('/help', {
+            templateUrl: 'views/help.html',
+            controller: 'noCtrl'
+        });
+
+        $routeProvider.when('/query', {
+            templateUrl: 'views/query.html',
+            controller: 'queryCtrl'
+        });
+
+        $routeProvider.when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'noCtrl'
+        });
+
+        $routeProvider.when('/recover', {
+            templateUrl: 'views/recover.html',
+            controller: 'recoverCtrl'
+        });
+
+        $routeProvider.when('/search', {
+            templateUrl: 'views/search.html',
+            controller: 'searchCtrl'
+        });
+
+        $routeProvider.when('/tutorial', {
+            templateUrl: 'views/tutorial.html',
+            controller: 'noCtrl'
+        });
+
+        $routeProvider.when('/download', {
+            templateUrl: 'views/download.html',
+            controller: 'noCtrl'
+        });
+
+        $routeProvider.when('/signin', {
+            templateUrl: 'views/signin.html',
+            controller: 'signinCtrl'
+        });
+        $routeProvider.when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'loginCtrl'
+        });
+       $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+}]).
+
+config(['$httpProvider', function ($httpProvider){
+    $httpProvider.interceptors.push( function($q, $window){
+        return {
+            'request': function (config) {
+                 config.headers = config.headers || {};
+                 if ($window.sessionStorage.token) {
+                     config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+                 }
+                 return config;
+            },
+            'response': function(response){
+                return response;
+            },
+            'responseError': function(rejection){
+                if(rejection.status == 401) {
+                    // Route to #/login
+                    location.replace('#/');
+                }
+                return $q.reject(rejection);
+            }
+        };
+    });
+}]);
+
+// Dédié aux pages n'ayant pas besoin de contrôleur (ex que du texte)
+angular.module('rgv').controller('userCtrl',
+    function ($scope,$rootScope, $log, Auth, User,$location) {
+});
+
+
+// Dédié aux pages n'ayant pas besoin de contrôleur (ex que du texte)
+angular.module('rgv').controller('noCtrl',
+    function ($scope,$rootScope, $log, Auth, User,$location) {
+
+});
+
+
+// Contrôleur de base associé à home.html
+angular.module('rgv').controller('appCtrl',
+    function ($scope,$rootScope, $log, Auth, User,$cookieStore, $location) {
+        $scope.msg = null;
+
+        var user = Auth.getUser();
+        if(user !== null && user !== undefined) {
+            var cookie_selectID =  $cookieStore.get('selectedID');
+           var cookie_jobID =  $cookieStore.get('jobID');
+           if (cookie_selectID != undefined && cookie_selectID != ''){
+            var user_seletedID = user.selectedID.split(',');
+            var cookies_list = cookie_selectID.split(',');
+            var job_list = cookie_jobID.split(',');
+            
+            for(var i=0;i<cookies_list.length; i++){
+              var index = user_seletedID.indexOf(cookies_list[i]);
+              if (index == -1){
+                user_seletedID.push(cookies_list[i]);
+              }
+            }
+            user.selectedID = user_seletedID.join(',');
+            user.$save({'uid': user.id}).then(function(data){
+                user = data;
+                $cookieStore.put('selectedID',null)
+            });
+           }
+
+           if (cookie_jobID != undefined && cookie_jobID != ''){
+              for(var i=0;i < job_list.length; i++){
+                var index = user_seletedID.indexOf(job_list[i]);
+                if (index > -1){
+                  user_seletedID.push(job_list[i]);
+                }
+              }
+              user.jobID = user_seletedID.join(',');
+              user.$save({'uid': user.id}).then(function(data){
+                  user = data;
+                  $cookieStore.put('jobID',null)
+              });
+           }
+           
+        }
+
+        if(user === null || user === undefined) {
+            User.is_authenticated({},{}).$promise.then(function(data){
+                $rootScope.$broadcast('loginCtrl.login', data);
+                Auth.setUser(data);
+            });
+
+        }
+});
+
+// Dans barre "query" permet de faire une recherche, pas encore implémenté dans RGV
+angular.module('rgv').controller('queryCtrl',
+    function ($scope,$rootScope, Auth, User, Search, $filter, ngTableParams) {
+});
+
+// Faire advanced research
+angular.module('rgv').controller('searchCtrl',
+    function($scope, $rootScope, $routeParams, $log, $location, $window, User, Auth, SearchHits) {
+        var hits = SearchHits.getHits();
+        console.log(hits)
+        $scope.nb_match = hits.hits.total
+        $scope.search_result = hits.hits.hits;
+});
+
+// Contrôleur dédié aux fonctions pour se logger
+angular.module('rgv').controller('loginCtrl',
+    function ($scope, $rootScope, $routeParams, $log, $location, $window, User, Auth, $cookieStore) {
+        $scope.msg = null;
+
+      var params = $location.search();
+
+      if(params['action'] !== undefined && params['action'] == 'confirm_email') {
+          User.confirm_email({}, {'token': params['token']}).$promise.then(function(data){
+              $scope.msg = data['msg'];
+          });
+      }
+      else if(params['token'] !== undefined) {
+          User.is_authenticated({},{'token': params['token']}).$promise.then(function(data){
+              $rootScope.$broadcast('loginCtrl.login', data);
+              Auth.setUser(data);
+              $window.sessionStorage.token = params['token'];
+              $location.search('token', null);
+              $location.path('');
+          });
+      }
+
+      $scope.remember = function(){
+        $cookieStore.put('remember','1');
+      }
+
+      $scope.recover = function() {
+        User.recover({},{'user_name': $scope.user_name}).$promise.then(function(data){
+            $scope.msg = data.msg;
+        });
+      }
+      
+      $scope.login = function() {
+
+          User.login({},{'user_name': $scope.user_name, 'user_password': $scope.user_password}).$promise.then(function(data){
+              if(data.token !== undefined){
+                 User.is_authenticated({},{'token': data.token}).$promise.then(function(data){
+                     $rootScope.$broadcast('loginCtrl.login', data);
+                     Auth.setUser(data);
+                     $window.sessionStorage.token = data.token;
+                     $location.search('token', null);
+                     $location.path('');
+                 });
+              }
+              else {
+                  $scope.msg = data.msg;
+              }
+          });
+    };
+});
+
+// Contrôleur dédié à la registration
+angular.module('rgv').controller('signinCtrl',
+    function ($scope,$rootScope, $log, Auth, User,$location) {
+        $scope.msg = null;
+
+        $scope.register = function(){
+        ////console.log("REGISTER");
+          if($scope.register_laboratory === null || $scope.register_laboratory == ''){
+              $scope.register_laboratory = 'No laboratory';
+          }
+          if($scope.register_country === null || $scope.register_country == ''){
+              $scope.register_country = 'No country';
+          }
+          if($scope.register_address === null || $scope.register_address == ''){
+              $scope.register_address = 'No address';
+          }
+          if($scope.register_email === null || $scope.register_email == ''){
+              $scope.msg = 'User email is empty and must be a valid email address';
+          }
+          if($scope.register_email === null || $scope.register_email == ''){
+              $scope.msg = 'User email is empty and must be a valid email address';
+          }
+          else if($scope.register_firstname === null || $scope.register_firstname == ''){
+              $scope.msg = 'User first name is empty';
+          }
+          else if($scope.register_lastname === null || $scope.register_lastname == ''){
+              $scope.msg = 'User last name is empty';
+          }
+          else if($scope.register_check_password === null || $scope.register_check_password == '' || $scope.register_check_password != $scope.register_password){
+              $scope.msg = 'Passwords do not match';
+          }
+          else {
+              User.register({},{'user_name': $scope.register_email,
+                                  'user_password':$scope.register_password,
+                                  'first_name': $scope.register_firstname,
+                                  'last_name': $scope.register_lastname,
+                                  'country':$scope.register_country,
+                                  'laboratory': $scope.register_laboratory,
+                                  'address': $scope.register_address,
+                              }).$promise.then(function(data){
+                  $scope.msg = data['msg'];
+              });
+          }
+      }
+});
+
+// Contrôleur pour retrouver mot de passe
+angular.module('rgv').controller('recoverCtrl',
+  function($scope, $rootScope, $routeParams, $log, $location, $window, User, Auth) {
+    var params = $location.search();
+    $scope.check_password = null;
+    $scope.password = null;
+    $scope.recover = function(){
+        if(params['token'] !== undefined) {
+            if($scope.check_password === null || $scope.check_password == '' || $scope.check_password != $scope.password){
+                $scope.msg = 'Passwords do not match';
+            }
+            else {
+                User.confirm_recover({},{ 'token': params['token'],'user_password': $scope.password}).$promise.then(function(data){
+                    $location.path('/login');
+                });
+            }
+        }
+        else {
+            $scope.msg = 'Invalid token confirmation';
+        }
+    };
+ });
+
+// Contrôleur pour affichage des informations d'un user
+angular.module('rgv').controller('userInfoCtrl',
+    function ($scope, $rootScope, $routeParams, $location, Auth, User) {
+        $scope.user = null;
+
+        $scope.projects=[];
+        $scope.studies=[];
+        $scope.signatures=[];
+        $scope.assays=[];
+
+        User.get({'uid': $routeParams['id']}).$promise.then(function(data){
+            $scope.user = data;
+        });
+
+        $scope.update = function() {
+            if($scope.user != null) {
+                $scope.user.$save({'uid': $routeParams['id']}).then(function(data){
+                    $scope.user = data;
+                });
+            }
+        }
+      $scope.auth_user = Auth.getUser();
+
+});
+
+// Service (pas contrôleur) d'identification
+angular.module('rgv').service('Auth', function() {
+    var user =null;
+    return {
+        getUser: function() {
+            return user;
+        },
+        setUser: function(newUser) {
+            user = newUser;
+        },
+        isConnected: function() {
+            return !!user;
+        }
+    };
+});
+
+// Service (pas un contrôleur) pour les recherches
+angular.module('rgv').service('SearchHits', function() {
+    var hits =null;
+    return {
+        getHits: function() {
+            return hits;
+        },
+        setHits: function(results) {
+            hits = results;
+        }
+    };
+});
