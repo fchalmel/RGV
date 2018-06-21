0138ba2eb3c3ba46c0c9ce8249a3c8d372c0bdb6
Thomas <dardethomas@gmail.com>
2018-06-21 11:09:01 +0200

    fix

--- rgv/webapp/app/scripts/resources.js
+++ rgv/webapp/app/scripts/resources.js
@@ -33,7 +33,7 @@
                 cache: false
             },
             read_stat: {
-                url: '/read_stat',
+                url: '/statgenome',
                 method: 'POST',
                 isArray: false,
                 cache: false
